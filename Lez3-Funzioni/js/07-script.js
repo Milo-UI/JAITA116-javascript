d8062ca2dcbf15ac4b046edd512113b43470b1d0
milo-ui-web-design <17987794+Milo-UI@users.noreply.github.com>
2024-03-12 17:00:48 +0100

    Update 07-script.js

--- Lez3-Funzioni/js/07-script.js
+++ Lez3-Funzioni/js/07-script.js
@@ -3,6 +3,11 @@
     Una volta scritto il Tweet e inviato, stampalo, sotto <hr> e resetta la textarea.
     Al click del bottone 'Cambia colore', il testo del Tweet cambia colore.
     Controlla che il Tweet non sia vuoto o composto da soli spazi.
+
+    ----------------------------
+    Facoltativo:
+    - Impostare un numero massimo di caratteri per il Tweet
+    - collegare un file css e rendere guardabile la pagina
 */
 
 let btnTweet = document.getElementById('btnTweet');
