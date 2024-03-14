d0494f0204cdb9a407cd89e1fa0ced3bcbeba8d0
milo-ui-web-design <17987794+Milo-UI@users.noreply.github.com>
2024-03-14 16:40:27 +0100

    Update 05-script.js

---
+++ Lez4-Object/js/05-script.js
@@ -0,0 +1,7 @@
+/* 
+    ESTRAZIONE DEL LOTTO
+
+    Creare uno script che faccia l'estrazione dei numeri vincenti del lotto.
+    - Estrarre in maniera casuale 6 numeri che siano compresi tra 1 e 90.
+    - Stampare sulla pagina HTML i 6 numeri con uno stile personalizzato.
+*/
