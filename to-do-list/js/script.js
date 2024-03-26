4e44f1fc0ed2b3ab1b437693554321630b02658c
milo-ui-web-design <17987794+Milo-UI@users.noreply.github.com>
2024-03-26 17:55:13 +0100

    Update script.js

--- to-do-list/js/script.js
+++ to-do-list/js/script.js
@@ -11,5 +11,8 @@
     Deve ovviamente essere presentabile dal punto di vista dell'aspetto.
     Opzionale: utilizzare icone per i bottoni e animazioni per l'aggiunta e la rimozione delle task:
         - Icone: https://fontawesome.com/
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+
         - Animazioni: https://animate.style/
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" referrerpolicy="no-referrer" />
 */
