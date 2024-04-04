87d36afc82fdbe96daf3f114ef8cb04480a23c12
milo-ui-web-design <17987794+Milo-UI@users.noreply.github.com>
2024-04-04 11:12:46 +0200

    pkmn

--- Lez7-Ajax/js/script04.js
+++ Lez7-Ajax/js/script04.js
@@ -1,9 +1,32 @@
-const URLUtenti = 'http://localhost:3000/utenti';
+const URLPOKEMON = 'http://localhost:3000/pokemon';
 
-fetch(URLUtenti)
+fetch(URLPOKEMON)
     .then(data => {
-        return data.json();
+        return data.json()
     })
-    .then(utenti => {
-        console.log(utenti);
+    .then(response => {
+        console.log(response);
+
+        creaCard(response);
     })
+
+let pokedex = document.querySelector('#pokedex');
+
+function creaCard(pokemonArray) {
+    pokemonArray.forEach(pokemon => {
+        let card = `
+            <div class="col-lg-3 col-md-6">
+                <div class="card">
+                    <img src = "${pokemon.sprite}" class="card-img-top" alt="${pokemon.nome}">
+                    <div class="card-body">
+                        <h5 class="card-title">${pokemon.nome}</h5>
+                        <p class="card-text">${pokemon.abilita}</p>
+                        <a href="#" class="btn btn-primary">Mostra Info</a>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        pokedex.innerHTML += card;
+    });
+}
