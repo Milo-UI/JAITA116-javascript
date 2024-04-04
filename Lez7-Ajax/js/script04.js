5c45f99820853c32f8c2c8c8b998a694159b382e
milo-ui-web-design <17987794+Milo-UI@users.noreply.github.com>
2024-04-04 13:00:08 +0200

    pokemon finito

--- Lez7-Ajax/js/script04.js
+++ Lez7-Ajax/js/script04.js
@@ -17,16 +17,45 @@ function creaCard(pokemonArray) {
         let card = `
             <div class="col-lg-3 col-md-6">
                 <div class="card">
-                    <img src = "${pokemon.sprite}" class="card-img-top" alt="${pokemon.nome}">
+                    <img src="${pokemon.sprite}" class="card-img-top" alt="${pokemon.nome}">
                     <div class="card-body">
                         <h5 class="card-title">${pokemon.nome}</h5>
                         <p class="card-text">${pokemon.abilita}</p>
-                        <a href="#" class="btn btn-primary">Mostra Info</a>
+                        <a href="#" class="btn btn-primary" id="pkmn${pokemon.id}" data-id="${pokemon.id}">Mostra Info</a>
                     </div>
                 </div>
             </div>
         `;
 
         pokedex.innerHTML += card;
+    });
+
+    mostraInfo(pokemonArray);
+}
+
+function mostraInfo(pokemon) {
+    let bottoni = document.querySelectorAll('.btn');
+
+    [...bottoni].forEach(btn => {
+        btn.addEventListener('click', function() {
+            let pInfo = document.createElement('p');
+
+            console.log(this.previousSibling.textContent.trim());
+
+            if (this.previousSibling.textContent.trim() == '') {
+                pInfo.textContent = pokemon[this.getAttribute('data-id')].info;
+                // Soluzione con filter() che funziona anche se l'ordine dell'array di pokemon è stato cambiato
+                // console.log(pokemon.filter(p => p.id == this.getAttribute('data-id'))[0].info);
+                this.parentNode.insertBefore(pInfo, this);
+                this.textContent = 'Nascondi Info';
+
+                this.parentNode.previousElementSibling.setAttribute('src', pokemon[this.getAttribute('data-id')].spriteSpalle);
+            } else {
+                this.previousSibling.remove();
+                this.textContent = 'Mostra Info';
+
+                this.parentNode.previousElementSibling.setAttribute('src', pokemon[this.getAttribute('data-id')].sprite);
+            }
+        })
     });
 }
