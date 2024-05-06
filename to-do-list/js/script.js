3cd4c4b70cc6205f90667dd5afb1557bc969e5b6
milo-ui-web-design <17987794+Milo-UI@users.noreply.github.com>
2024-05-06 12:25:57 +0200

    to do

--- to-do-list/js/script.js
+++ to-do-list/js/script.js
@@ -16,3 +16,56 @@
         - Animazioni: https://animate.style/
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" referrerpolicy="no-referrer" />
 */
+
+var btnAggiungi = document.querySelector('#btnAggiungi');
+var lista = document.querySelector('#lista');
+
+function aggiungi() {
+    var item = document.querySelector('#item').value;
+    var data = document.querySelector('#data').value;
+
+    var task = document.createElement('li');
+    task.setAttribute('class', 'animate__animated animate__fadeInRight');
+
+    var check = document.createElement('input');
+    check.setAttribute('type', 'checkbox');
+    check.classList.add('done');
+    task.appendChild(check);
+
+    task.innerHTML += ' <span>' + item + '</span> - <span>' + data + '</span>';
+
+    lista.appendChild(task);
+
+    document.querySelector('#item').value = '';
+    document.querySelector('#data').value = '';
+
+    var btnModify = document.createElement('button');
+    btnModify.innerHTML = '<i class="fa-solid fa-marker"></i>';
+    btnModify.setAttribute('class', 'btn-mod');
+    task.appendChild(btnModify);
+
+    btnModify.addEventListener('click', modifica);
+
+    function modifica() {
+        rimuovi();
+        document.querySelector('#item').value = item;
+        document.querySelector('#data').value = data;
+    }
+
+    var btnRemove = document.createElement('button');
+    btnRemove.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    btnRemove.setAttribute('class', 'btn-rem');
+    task.appendChild(btnRemove);
+
+    btnRemove.addEventListener('click', rimuovi);
+
+    function rimuovi() {
+        task.classList.add('animate__animated', 'animate__fadeOutRight');
+
+        setTimeout(() => {
+            lista.removeChild(task);
+        }, 500);
+    }
+}
+
+btnAggiungi.addEventListener('click', aggiungi);
